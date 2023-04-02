77ea98771b6212eaf4d25c7f829f06290ddbee9b
Thomas Haller <thomashaller@gmx.at>
2023-04-02 17:57:18 +0000

    fixed imports for public_key_to_address tests

--- crates/ethcore/src/ethereum/public_key_to_address.rs
+++ crates/ethcore/src/ethereum/public_key_to_address.rs
@@ -1,5 +1,3 @@
-
-
 use ethereum_types::{Address, H512};
 use tiny_keccak::Hasher;
 
@@ -19,13 +17,20 @@ pub fn public_key_to_address(public_key: &H512) -> Address {
     Address::from(address)
 }
 
-#[test]
-fn test_public_key_to_address() {
-    let public_key :H512 = H512::from_str("b461fffc33fed96f525b6791c9f628d92d9d06220d3150b46246ab2f107f4459139533435e39629a896d3e8fc9bbce1d63fc652429d1e2ca261cd21f119cae87").expect("invalid public key");
-    let expected_address =
-        Address::from_str("ffd50c8d035462d15c06413d9dff638bab201180").expect("invalid address");
+#[cfg(test)]
+mod test {
+    use std::str::FromStr;
+    use ethereum_types::{H512, Address};
+    use crate::ethereum::public_key_to_address::public_key_to_address;
 
-    let address = public_key_to_address(&public_key);
+    #[test]
+    fn test_public_key_to_address() {
+        let public_key :H512 = H512::from_str("b461fffc33fed96f525b6791c9f628d92d9d06220d3150b46246ab2f107f4459139533435e39629a896d3e8fc9bbce1d63fc652429d1e2ca261cd21f119cae87").expect("invalid public key");
+        let expected_address =
+            Address::from_str("ffd50c8d035462d15c06413d9dff638bab201180").expect("invalid address");
 
-    assert_eq!(address, expected_address);
+        let address = public_key_to_address(&public_key);
+
+        assert_eq!(address, expected_address);
+    }
 }
