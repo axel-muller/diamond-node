9b9d3b9a01d38db41814131842f9f4893bf0d483
Thomas Haller <thomashaller@gmx.at>
2023-11-19 21:40:09 +0100

    EarlyEpochEndManager Impl

--- crates/ethcore/src/engines/hbbft/hbbft_early_epoch_end_manager.rs
+++ crates/ethcore/src/engines/hbbft/hbbft_early_epoch_end_manager.rs
@@ -17,3 +17,25 @@ pub(crate) struct EarlyEpochEndManager {
     validators: Vec<NodeId>,
 
 }
+
+
+impl EarlyEpochEndManager { 
+
+    // new
+    pub fn new(allowed_devp2p_warmup_time: Duration) -> Self {
+        Self {
+            current_tracked_epoch_number: 0,
+            start_time: Instant::now(),
+            allowed_devp2p_warmup_time: allowed_devp2p_warmup_time,
+            validators: Vec::new(),
+        }
+    }
+
+    pub fn notify_new_epoch(&mut self, epoch: u64, validators: Vec<NodeId> ) {
+
+        self.current_tracked_epoch_number = epoch;
+        self.validators = validators;
+        self.start_time = Instant::now();
+    }
+
+}
