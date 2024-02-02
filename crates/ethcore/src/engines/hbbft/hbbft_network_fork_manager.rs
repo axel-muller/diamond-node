b3d21363cc111b9950bde11acf59e2b8894dade8
Thomas Haller <thomashaller@gmx.at>
2024-02-02 19:23:49 +0100

    fork manager: further drafts for creating the fork.

---
+++ crates/ethcore/src/engines/hbbft/hbbft_network_fork_manager.rs
@@ -0,0 +1,48 @@
+use std::collections::VecDeque;
+
+use ethereum_types::Address;
+
+
+struct HbbftFork {
+    start_timestamp: u64,
+    start_block: u64,
+    is_finished: bool,
+    end_timestamp: u64,
+    end_block: u64,
+    validator_set: Vec<Address>,
+}
+
+
+/// Hbbft network fork manager.
+/// This manager is responsible for managing the forks.
+/// It allows cheap queries to see if a Fork is pending,
+/// and stores information about a fork that is finished.
+pub struct HbbftNetworkForkManager {
+
+    /// If a fork is currently in progress, this is true.
+    is_currently_forking: bool,
+
+    /// a ordered list with upcomming forks.
+    finished_forks: VecDeque<HbbftFork>,
+
+    /// a ordered list with upcomming forks, including a fork that is in progress.
+    /// see @is_currently_forking for more information.
+    pending_forks: VecDeque<HbbftFork>,
+}
+
+
+impl HbbftNetworkForkManager {
+
+
+
+    /// Returns None if not forking
+    /// Returns a List of Addresses that become the new validator set and
+    /// declares the fork as active, 
+    pub fn should_fork(&mut self,last_block_number: u64,  last_block_time_stamp: u64) -> Option<Vec<Address>> {
+        // fields omitted
+
+
+        None
+    }
+
+}
