0accd00b4419362629488eafbfa135b252772a98
David Forstenlechner <dforsten@gmail.com>
2025-03-02 21:37:46 +0100

    Reworked comments to be more concise and accurate

--- crates/ethcore/src/engines/hbbft/utils/transactions_shuffling.rs
+++ crates/ethcore/src/engines/hbbft/utils/transactions_shuffling.rs
@@ -24,19 +24,18 @@ fn address_xor_u256(address: &Address, seed: U256) -> Address {
     Address::from(result)
 }
 
-/// transactions is expected to be free of duplicates. This is no guarantee that that transactions with the same nonce
-/// but different content are present in the given transactions. There is also no guarantee the transactions are sorted
-/// by nonce.
-/// Avoid using implementations not under our control, avoid adding dependencies on crates or standard library functions
-/// which may change in future versions.
-/// The implementation needs to be both portable and deterministic.6
+/// The list of transactions is expected to be free of duplicates.
 fn deterministic_transactions_shuffling(
     transactions: Vec<SignedTransaction>,
     seed: U256,
 ) -> Vec<SignedTransaction> {
+    // The implementation needs to be both portable and deterministic.
+    // There is no guarantee that the input list of transactions does not contain transactions
+    // with the same nonce but different content.
+    // There is also no guarantee the transactions are sorted by nonce.
+
     // Group transactions by sender.
     // * Walk the transactions from first to last
-    // * Add unique senders to a vector in the order they appear in the transactions list
     // * Add transactions with unique nonce to a per-sender vector
     //   * Discard transactions with a nonce already existing in the list of transactions
     let mut txs_by_sender: HashMap<_, Vec<SignedTransaction>> = HashMap::new();
@@ -55,20 +54,19 @@ fn deterministic_transactions_shuffling(
 
     // For each sender, sort their transactions by nonce (lowest first).
     // Nonces are expected to be unique at this point, guaranteeing portable
-    // and deterministic results independent from the sorting algorithm as long as
+    // and deterministic results independent of the sorting algorithm as long as
     // the sorting algorithm works and is implemented correctly.
     for txs in txs_by_sender.values_mut() {
         txs.sort_by_key(|tx| tx.tx().nonce);
     }
 
-    // Randomly shuffle the list of senders in the order they appear in the transactions list.
-    // Use a portable and deterministic random number generator where we control the exact implementation.
-    // * Seed the random number generator with the given seed value, identical for all validators
-    // * Use the random number generator to pick a sender in the unique sender list
-    // * Remove the sender from the sender list
-    //   * Assure sender removal does not change the sequence of senders
-    // * Add the removed sender to a new list
-    // * Keep moving randomly selected senders until the original sender list is empty.
+    // Deterministically randomize the order of senders.
+    // Same as with transactions we rely on the uniqueness of list members and
+    // a properly functioning sorting algorithm. To prevent predictable order we
+    // XOR each sender address with the random number generated through the HBBFT
+    // protocol, and use the resulting address as sorting key.
+    // The random number is guaranteed to be identical for all validators at the
+    // time of block creation.
     let mut senders: Vec<_> = txs_by_sender.keys().cloned().collect();
     senders.sort_by_key(|address| address_xor_u256(address, seed));
 
@@ -84,7 +82,6 @@ fn deterministic_transactions_shuffling(
     final_transactions
 }
 
-// Write a test function to test the address_xor_u256 function with known seeds and addresses and known XOR results
 #[cfg(test)]
 mod tests {
     use super::*;
